80969940564943262dc4bee6dc6e63cf743fc3f7
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-01-25 15:31:21 +0000

    3 more pages created, bootstrap and navbar added, linked up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def schedule
+  end
+
+  def myschedule
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # get 'home/index'
   root'home#index'
+
+  get 'home/schedule'
+
+  get 'home/myschedule'
+
+  get 'home/contact'
+
 end
