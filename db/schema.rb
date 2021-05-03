2a872f4c187320c1ff407b55c8c6fe3cac195530
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-05-03 13:29:12 +0100

    devise stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_090528) do
+ActiveRecord::Schema.define(version: 2021_04_30_105839) do
 
   create_table "talks", force: :cascade do |t|
     t.string "speaker"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_04_09_090528) do
     t.string "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_talks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
