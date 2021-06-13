44295e0b7691c9f661ebae8bf8365a284ff2a262
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-06-13 12:01:38 +0100

    poster pages created and linked

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   devise_for :users
   resources :talks
   resources :contact, only: [:create]
-  # get 'home/index'
+
   root'home#index'
 
   get 'home/keynote'
@@ -12,5 +14,60 @@ Rails.application.routes.draw do
   get 'home/contact'
 
   get 'home/poster'
+
+  get 'home/poster1'
+
+  get 'home/poster2'
+
+  get 'home/poster3'
+
+  get 'home/poster4'
+
+  get 'home/poster5'
+
+  get 'home/poster6'
+
+  get 'home/poster7'
+
+  get 'home/poster8'
+
+  get 'home/poster9'
+
+  get 'home/poster10'
+
+  get 'home/poster11'
+
+  get 'home/poster12'
+
+  get 'home/poster13'
+
+  get 'home/poster14'
+
+  get 'home/poster15'
+
+  get 'home/poster16'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+  get 'home/symposium1'
+
+
+
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_105839) do
+ActiveRecord::Schema.define(version: 2021_06_12_155036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "poster_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "talks", force: :cascade do |t|
     t.string "speaker"
