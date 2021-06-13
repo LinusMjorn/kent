014536fe9ecfdf0ce385525035a8d2b9e437317d
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-06-13 12:47:24 +0100

    all pages now created and linked

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -59,4 +59,73 @@ class HomeController < ApplicationController
   def poster16
   end
 
+  def symposium1
+  end
+
+  def symposium2
+  end
+
+  def symposium3
+  end
+
+  def symposium4
+  end
+
+  def symposium5
+  end
+
+  def symposium6
+  end
+
+  def symposium7
+  end
+
+  def symposium8
+  end
+
+  def symposium9
+  end
+
+  def session1
+  end
+  
+  def session2
+  end
+
+  def session3
+  end
+
+  def session4
+  end
+
+  def session5
+  end
+
+  def session6
+  end
+
+  def session7
+  end
+  
+  def session8
+  end
+
+  def session9
+  end
+
+  def quiz
+  end
+  
+  def panel
+  end
+
+  def key1
+  end
+
+  def key2
+  end
+
+  def key3
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,21 +49,47 @@ Rails.application.routes.draw do
 
   get 'home/symposium1'
 
-  get 'home/symposium1'
+  get 'home/symposium2'
 
-  get 'home/symposium1'
+  get 'home/symposium3'
 
-  get 'home/symposium1'
+  get 'home/symposium4'
 
-  get 'home/symposium1'
+  get 'home/symposium5'
 
-  get 'home/symposium1'
+  get 'home/symposium6'
 
-  get 'home/symposium1'
+  get 'home/symposium7'
 
-  get 'home/symposium1'
+  get 'home/symposium8'
 
-  get 'home/symposium1'
+  get 'home/session1'
+
+  get 'home/session2'
+
+  get 'home/session3'
+
+  get 'home/session4'
+
+  get 'home/session5'
+
+  get 'home/session6'
+
+  get 'home/session7'
+
+  get 'home/session8'
+
+  get 'home/session9'
+
+  get 'home/quiz'
+
+  get 'home/panel'
+
+  get 'home/key1'
+
+  get 'home/key2'
+
+  get 'home/key3'
 
 
 
